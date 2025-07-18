a669bd024c457c54cf2b2f764c1c1227c62bad95
Angilio <johntroaikkyman@gmail.com>
2025-07-18 22:30:18 +0300

    Change layouts.app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,7 +71,5 @@
             </footer>
         </div>
 
-        <!-- ✅ Bootstrap 5 JS (via CDN) -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
